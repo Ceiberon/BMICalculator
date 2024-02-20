6dbb2f84fced3f3b80ca44045fff38492833cc52
Ceiberon <ethem_641@hotmail.com>
2024-02-20 17:57:09 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int kg;
+        double boy;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Kilonuzu girin: ");
+        kg = input.nextInt();
+
+        System.out.print("Boyunuzu girin (Metre Cinsinden): ");
+        boy = input.nextDouble();
+
+        double vucutKitleEndeksi = kg / (boy * boy)*10000; // Doğru formül burada kullanıldı.
+
+        System.out.print("Vücut Kitle Endeksiniz: " + vucutKitleEndeksi);
+    }
+}
